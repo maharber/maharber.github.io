7f1a84777f776f8fa1cf731562b552b996b2eef2
maharber <40210486+maharber@users.noreply.github.com>
2018-06-15 09:02:18 -0700

    Create personalSite.js

---
+++ personalSite.js
@@ -0,0 +1,8 @@
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidebar").style.height = "150px";
+}
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidebar").style.height = "30px";
+}
